e6f1f0d0d8abb930777bbca6e53605c642407a66
milka <mileto94@abv.bg>
2014-04-01 14:42:27 +0300

    Add cls Animal

--- cls_animal.py
+++ cls_animal.py
@@ -1,6 +1,14 @@
-# class Animal():
-#     """docstring for Animal"""
-#     def __init__(self, species, name, age, gender, weight):
-#         self.species = species
-#         self.name = name
-#         self.age = age
+class Animal():
+    """docstring for Animal"""
+    def __init__(self, species, name, age, gender, weight):
+        self.species = species
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.weight = weight
+
+    def eat(self, food_per_day):
+        return self.weight + food_per_day
+
+    def grow(self):
+        return self.age + 1
